2c488569ab7c405cdae5bb667eb36114d326bcee
Josh Karpel <josh.karpel@gmail.com>
2021-09-24 23:24:53 -0400

    add yargs

---
+++ tlox/cli.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import yargs from "yargs"
+import { hideBin } from "yargs/helpers"
+
+yargs(hideBin(process.argv))
+  .scriptName("tlox")
+  .command(
+    ["run <path>", "$0"],
+    "Run a tlox script.",
+    (yargs) => {
+      return yargs.positional("path", {
+        describe: "The path to the script.",
+        type: "string",
+        normalize: true,
+      })
+    },
+    (argv) => {
+      console.log(`Path: ${argv.path}`)
+    },
+  )
+  .parse()
